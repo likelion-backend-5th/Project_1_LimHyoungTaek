9400a98efbc617f0e425b92948b63b517f8bd0ce
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-30 15:17:16 +0900

    feat: add ItemService

---
+++ src/main/java/com/be05/market/service/ItemService.java
@@ -0,0 +1,11 @@
+package com.be05.market.service;
+
+import com.be05.market.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+}
