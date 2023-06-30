cc9f3792528717e87dcab4e002177e6766e14a99
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-30 15:04:36 +0900

    feat: init files

--- src/main/java/com/be05/market/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.be05.market.repository;
-
-import com.be05.market.entity.ItemEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository
-        extends JpaRepository<ItemEntity, Long> {
-}
