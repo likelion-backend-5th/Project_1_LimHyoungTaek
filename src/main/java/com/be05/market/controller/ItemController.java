db7486b55b847a49c2f95af520bb165c05ea01a0
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-30 17:04:57 +0900

    feat: Implementing 'POST /items' - RequestBody 우선 구현, ResponseBody는 아직 미구현

--- src/main/java/com/be05/market/controller/ItemController.java
+++ src/main/java/com/be05/market/controller/ItemController.java
@@ -1,10 +1,10 @@
 package com.be05.market.controller;
 
+import com.be05.market.dto.SalesItem;
 import com.be05.market.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -14,6 +14,11 @@ public class ItemController {
     private final ItemService itemService;
 
     // TODO: POST /items
+    @PostMapping("")
+    public SalesItem create(@RequestBody SalesItem items) {
+        return itemService.createItem(items);
+    }
+
     // TODO: GET /items?page={page}&limit={limit}
     // TODO: GET /items/{itemId}
     // TODO: PUT /items/{itemId}
--- src/main/java/com/be05/market/service/ItemService.java
+++ src/main/java/com/be05/market/service/ItemService.java
@@ -1,5 +1,7 @@
 package com.be05.market.service;
 
+import com.be05.market.dto.SalesItem;
+import com.be05.market.entity.ItemEntity;
 import com.be05.market.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
+
+    public SalesItem createItem(SalesItem items) {
+        ItemEntity newItems = new ItemEntity();
+        newItems.setTitle(items.getTitle());
+        newItems.setDescription(items.getDescription());
+        newItems.setMinPriceWanted(items.getMinPriceWanted());
+        newItems.setWriter(items.getWriter());
+        newItems.setPassword(items.getPassword());
+        newItems.setStatus("판매중");
+        return SalesItem.fromEntity(itemRepository.save(newItems));
+    }
 }
