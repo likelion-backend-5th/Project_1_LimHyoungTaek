9f925f5dabbaf26b3bbf1fe2571d915ac5c9d21f
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-03 14:51:12 +0900

    modified: GET /items/{itemId}  - Output all column -> a few column(title, description, minPriceWanted, status)

---
+++ src/main/java/com/be05/market/dto/ContentInfoDto.java
@@ -0,0 +1,19 @@
+package com.be05.market.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ContentInfoDto {
+    private String title;
+    private String description;
+    private Long minPriceWanted;
+    private String status;
+
+    public ContentInfoDto(SalesItemDto items) {
+        this.title = items.getTitle();
+        this.description = items.getDescription();
+        this.minPriceWanted = items.getMinPriceWanted();
+        this.status = items.getStatus();
+    }
+}
