4df5e9c824a45c0b1231a94f581142424d89de80
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-03 16:40:25 +0900

    modified: CommentDto.java  - POST /items/{itemId}/comments  - GET /items/{itemId}/comments

--- src/main/java/com/be05/market/controller/CommentController.java
+++ src/main/java/com/be05/market/controller/CommentController.java
@@ -1,8 +1,10 @@
 package com.be05.market.controller;
 
+import com.be05.market.dto.CommentDto;
 import com.be05.market.dto.ResponseDto;
 import com.be05.market.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,7 +15,20 @@ public class CommentController {
     private final ResponseDto responseDto = new ResponseDto();
 
     // TODO: POST /items/{itemId}/comments
+    @PostMapping("/comments")
+    public ResponseDto create(@PathVariable("itemId") Long itemId,
+                             @RequestBody CommentDto commentDto) {
+        commentService.postComment(itemId, commentDto);
+        responseDto.setMessage("댓글이 등록되었습니다.");
+        return responseDto;
+    }
     // TODO: GET /items/{itemId}/comments
+    @GetMapping("/comments")
+    public Page<CommentDto> readAllReply(@PathVariable("itemId") Long itemId) {
+        return commentService.getCommentsPaged(itemId);
+    }
+
+
     // TODO: PUT /items/{itemId}/comments/{commentId}
     // TODO: PUT /items/{itemId}/comments/{commentId}/reply
     // TODO: DELETE /items/{itemId}/comments/{commentId}
