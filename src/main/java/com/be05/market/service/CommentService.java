12ecd6da1cf2c36c238d56bc3f5f11c0becf211c
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-04 04:02:26 +0900

    ✨ modified: CommentService.java  1. Page<CommentPageInfoDto>를 리턴타입으로 받아오면서 ResponseBody 형식 변경  2. validateCommentByItemId() 생성 -> 각 메서드마다 요청 댓글 유무, 대상 댓글이 대상 게시글의 댓글인지 확인하는 과정이 겹쳐서  3. validatePassword() -> 비밀번호 확인 인터페이스  4. 답글(Reply)은 물품 등록 작성자만 달 수 있다.   - 그래서 여러가지 수정 후 자세한 사항은 주석에 작성하였다.

--- src/main/java/com/be05/market/service/CommentService.java
+++ src/main/java/com/be05/market/service/CommentService.java
@@ -1,7 +1,7 @@
 package com.be05.market.service;
 
 import com.be05.market.dto.CommentDto;
-import com.be05.market.dto.SalesItemDto;
+import com.be05.market.dto.mapping.CommentPageInfoDto;
 import com.be05.market.entity.CommentEntity;
 import com.be05.market.entity.ItemEntity;
 import com.be05.market.repository.CommentRepository;
@@ -16,16 +16,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final ItemRepository itemRepository;
+    private final ItemService itemService;
     private final CommentRepository commentRepository;
 
-    // Post Comment, Reply?
+    // Post Comment
     public void postComment(Long itemId, CommentDto comments) {
         if (!itemRepository.existsById(itemId))
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
@@ -40,75 +39,62 @@ public class CommentService {
     }
 
     // View All Comments
-    public Page<CommentDto> getCommentsPaged(Long itemId) {
+    public Page<CommentPageInfoDto> getCommentsPaged(Long itemId) {
         if (!itemRepository.existsById(itemId))
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         Pageable pageable =
                 PageRequest.of(0, 25, Sort.by("id"));
         Page<CommentEntity> commentEntities = commentRepository.findAll(pageable);
-        return commentEntities.map(CommentDto::fromEntity);
+        return commentEntities.map(CommentPageInfoDto::fromEntity);
     }
 
     // Modifying Comment
     public void modifiedComment(Long commentId, Long itemId, CommentDto comments) {
-        // 요청 댓글 존재 유무
-        Optional<CommentEntity> optionalComment = commentRepository.findById(commentId);
-
-        // 존재 X -> exception
-        if (optionalComment.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-
-        // 대상 댓글 == 대상 게시글의 댓글인지
-        CommentEntity comment = optionalComment.get();
-        if (!itemId.equals(comment.getItemId()))
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-
-        comment.setContent(comments.getContent());
-        CommentDto.fromEntity(commentRepository.save(comment));
+        CommentEntity commentEntity = validateCommentByItemId(commentId, itemId);
+        commentEntity.validatePassword(comments.getPassword());
+        commentEntity.setContent(comments.getContent());
+        CommentDto.fromEntity(commentRepository.save(commentEntity));
     }
 
-    // Modifying Reply
-    public void modifiedReply(Long commentId, Long itemId, CommentDto comments) {
-        // 요청 댓글 존재 유무
-        Optional<CommentEntity> optionalComment = commentRepository.findById(commentId);
+    // Post, Modifying Reply
+    public void modifiedReply(Long commentId, Long itemId, CommentDto comments)
+    {
+        CommentEntity commentEntity = validateCommentByItemId(commentId, itemId);
+        ItemEntity itemEntity = itemService.getItemById(itemId);
 
-        // 존재 X -> exception
-        if (optionalComment.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-
-        // 대상 댓글 == 대상 게시글의 댓글인지
-        CommentEntity comment = optionalComment.get();
-        if (!itemId.equals(comment.getItemId()))
+        // 1. 답글 작성자 != 물품 등록 작성자 -> 예외 처리
+        // 댓글에 답글을 달 수 있는 사용자는 물품 정보를 등록한 사용자 뿐
+        if(!itemEntity.getWriter().equals(comments.getWriter()))
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
 
-        comment.setReply(comments.getReply());
-        CommentDto.fromEntity(commentRepository.save(comment));
+        // 2. 물품 등록 작성자 == 답글 작성자 라는건 위의 예외에서 증명
+        // 만약 댓글이 등록된 대상 물품을 등록한 사람일 경우
+        // -> 물품 등록 == 댓글 == 답글 다 같은 작성자이다.
+        if (commentEntity.getWriter().equals(comments.getWriter())){
+            // 물품을 등록할 때 사용한 비밀번호를 첨부할 경우 답글 항목을 수정할 수 있다.
+            // 물품 등록 비밀번호 != 답글 비밀번호 -> 예외 처리
+            itemEntity.validatePassword(comments.getPassword());
+        }
+
+        commentEntity.setReply(comments.getReply());
+        CommentDto.fromEntity(commentRepository.save(commentEntity));
     }
 
     // Delete Comment
-    public void deleteComment(Long commentId, Long itemId) {
-        Optional<CommentEntity> optionalComment = commentRepository.findById(commentId);
-
-        if (optionalComment.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-
-        CommentEntity comment = optionalComment.get();
-        if (!itemId.equals(comment.getItemId()))
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-
+    public void deleteComment(Long commentId, Long itemId, CommentDto comments) {
+        CommentEntity commentEntity = validateCommentByItemId(commentId, itemId);
+        commentEntity.validatePassword(comments.getPassword());
         commentRepository.deleteById(commentId);
     }
 
-    // TODO: 따로 정리해서 가져다 쓰기
-//    // Import item entities with ID
-//    public ItemEntity getItemById(Long id) {
-//        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
-//    }
-//
-//    // Check Password
-//    private void validPW(ItemEntity itemEntity, String password) {
-//        if (!itemEntity.getPassword().equals(password)) {
-//            throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
-//        }
-//    }
+    // 요청 댓글 유무, 대상 댓글이 대상 게시글의 댓글인지 확인
+    public CommentEntity validateCommentByItemId(Long commentId, Long itemId) {
+        CommentEntity commentEntity = commentRepository.findById(commentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (!itemId.equals(commentEntity.getItemId()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        return commentEntity;
+    }
 }
