c4906820f0048bba3ec38380e6879e943e20459b
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-03 16:41:31 +0900

    modified: CommentService.java  - postComment: POST /items/{itemId}/comments  - getCommentsPaged: GET /items/{itemId}/comments

--- src/main/java/com/be05/market/service/CommentService.java
+++ src/main/java/com/be05/market/service/CommentService.java
@@ -2,8 +2,7 @@ package com.be05.market.service;
 
 import com.be05.market.dto.CommentDto;
 import com.be05.market.dto.SalesItemDto;
-import com.be05.market.dto.salesitem.PageInfoDto;
-import com.be05.market.entity.ItemEntity;
+import com.be05.market.entity.CommentEntity;
 import com.be05.market.repository.CommentRepository;
 import com.be05.market.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,27 +13,37 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommentService {
+    private final ItemRepository itemRepository;
     private final CommentRepository commentRepository;
 
     // Post Comment, Reply?
-    public void postComment(CommentDto comments) {
+    public void postComment(Long itemId, CommentDto comments) {
+        if (!itemRepository.existsById(itemId))
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        CommentEntity newComment = new CommentEntity();
+        newComment.setItemId(itemId);
+        newComment.setWriter(comments.getWriter());
+        newComment.setPassword(comments.getPassword());
+        newComment.setContent(comments.getContent());
+        CommentDto.fromEntity(commentRepository.save(newComment));
+        log.info(String.valueOf(newComment));
     }
 
     // View All Comments
-    public Page<PageInfoDto> getCommentsPaged(Integer page, Integer limit) {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public Page<CommentDto> getCommentsPaged(Long itemId) {
+        if (!itemRepository.existsById(itemId))
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        Pageable pageable =
+                PageRequest.of(0, 25, Sort.by("id"));
+        Page<CommentEntity> commentEntities = commentRepository.findAll(pageable);
+        return commentEntities.map(CommentDto::fromEntity);
     }
 
     // Modifying Comment
