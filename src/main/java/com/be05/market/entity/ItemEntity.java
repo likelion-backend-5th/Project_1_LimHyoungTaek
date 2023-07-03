2167ebc13ff94d8f8c51a2e921de4871c84044b1
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-03 14:07:06 +0900

    feat: Add validation

--- src/main/java/com/be05/market/entity/ItemEntity.java
+++ src/main/java/com/be05/market/entity/ItemEntity.java
@@ -1,6 +1,7 @@
 package com.be05.market.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -10,11 +11,22 @@ public class ItemEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "제목을 작성해주세요.")
     private String title;
+
+    @NotNull(message = "등록한 물품에 대한 설명을 작성해주세요.")
     private String description;
+
     private String imageURL;
+
+    @NotNull(message = "등록한 물품에 대한 최소 가격을 작성해주세요.")
     private Long minPriceWanted;
+
     private String status;
+
+    @NotNull(message = "작성자를 작성해주세요.")
     private String writer;
+
     private String password;
 }
