ec2fa01ed567cb2573fa187b455f8d1e0f236330
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-03 16:39:10 +0900

    feat: 오타 수정(ItemEntity -> CommentEntity)

--- src/main/java/com/be05/market/repository/CommentRepository.java
+++ src/main/java/com/be05/market/repository/CommentRepository.java
@@ -1,7 +1,7 @@
 package com.be05.market.repository;
 
-import com.be05.market.entity.ItemEntity;
+import com.be05.market.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<ItemEntity, Long> {
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
 }
