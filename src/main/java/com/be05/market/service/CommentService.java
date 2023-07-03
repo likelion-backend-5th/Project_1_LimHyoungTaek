67bdf99ae3eb5de8bb7501d7a65b965c2cd666e8
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-03 17:31:40 +0900

    modified: CommentController.java  - modifiedComment: PUT /items/{itemId}/comments/{commentId}  - modifiedReply: PUT /items/{itemId}/comments/{commentId}/reply  - deleteComment: DELETE /items/{itemId}/comments/{commentId}

--- src/main/java/com/be05/market/service/CommentService.java
+++ src/main/java/com/be05/market/service/CommentService.java
@@ -3,6 +3,7 @@ package com.be05.market.service;
 import com.be05.market.dto.CommentDto;
 import com.be05.market.dto.SalesItemDto;
 import com.be05.market.entity.CommentEntity;
+import com.be05.market.entity.ItemEntity;
 import com.be05.market.repository.CommentRepository;
 import com.be05.market.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -47,13 +50,53 @@ public class CommentService {
     }
 
     // Modifying Comment
-    public void modifiedComment(Long id, SalesItemDto item) {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public void modifiedComment(Long commentId, Long itemId, CommentDto comments) {
+        // 요청 댓글 존재 유무
+        Optional<CommentEntity> optionalComment = commentRepository.findById(commentId);
+
+        // 존재 X -> exception
+        if (optionalComment.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        // 대상 댓글 == 대상 게시글의 댓글인지
+        CommentEntity comment = optionalComment.get();
+        if (!itemId.equals(comment.getItemId()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        comment.setContent(comments.getContent());
+        CommentDto.fromEntity(commentRepository.save(comment));
+    }
+
+    // Modifying Reply
+    public void modifiedReply(Long commentId, Long itemId, CommentDto comments) {
+        // 요청 댓글 존재 유무
+        Optional<CommentEntity> optionalComment = commentRepository.findById(commentId);
+
+        // 존재 X -> exception
+        if (optionalComment.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        // 대상 댓글 == 대상 게시글의 댓글인지
+        CommentEntity comment = optionalComment.get();
+        if (!itemId.equals(comment.getItemId()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        comment.setReply(comments.getReply());
+        CommentDto.fromEntity(commentRepository.save(comment));
     }
 
     // Delete Comment
-    public void deleteComment(Long id, SalesItemDto item) {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public void deleteComment(Long commentId, Long itemId) {
+        Optional<CommentEntity> optionalComment = commentRepository.findById(commentId);
+
+        if (optionalComment.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        CommentEntity comment = optionalComment.get();
+        if (!itemId.equals(comment.getItemId()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        commentRepository.deleteById(commentId);
     }
 
     // TODO: 따로 정리해서 가져다 쓰기
