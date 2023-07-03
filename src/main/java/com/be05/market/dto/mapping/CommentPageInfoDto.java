fb68cd1a644e3ce2af285f8c3889f4f90c0e545c
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-04 03:53:47 +0900

    feat: add CommentPageInfoDto.java  - dto/mapping에 Page의 ResponseBody 부분을 맡고있는 Dto를 넣어둠  - ItemPageInfoDto와 구분

---
+++ src/main/java/com/be05/market/dto/mapping/CommentPageInfoDto.java
@@ -0,0 +1,22 @@
+package com.be05.market.dto.mapping;
+
+import com.be05.market.entity.CommentEntity;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CommentPageInfoDto {
+    private Long id;
+    private String content;
+    private String reply;
+
+    public static CommentPageInfoDto fromEntity(CommentEntity entity) {
+        CommentPageInfoDto infoDto = new CommentPageInfoDto();
+        infoDto.setId(entity.getId());
+        infoDto.setContent(entity.getContent());
+        infoDto.setReply(entity.getReply());
+        return infoDto;
+    }
+}
