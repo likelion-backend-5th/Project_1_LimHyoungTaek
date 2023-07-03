a1c0a5b39062ffdf3c634c539e9ae560563f2513
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-03 14:52:14 +0900

    modified: GET /items?page={page}&limit={limit}  - Output all column -> a few column(id, title, description, minPriceWanted, status)  - imageUrl -> add @JsonInclude(JsonInclude.Include.NON_NULL) Null 값 일때 미출력

---
+++ src/main/java/com/be05/market/dto/PageInfoDto.java
@@ -0,0 +1,28 @@
+package com.be05.market.dto;
+
+import com.be05.market.entity.ItemEntity;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PageInfoDto {
+    private Long id;
+    private String title;
+    private String description;
+    private Long minPriceWanted;
+    private String imageUrl;
+    private String status;
+
+    public static PageInfoDto fromEntity(ItemEntity entity) {
+        PageInfoDto infoDto = new PageInfoDto();
+        infoDto.setId(entity.getId());
+        infoDto.setTitle(entity.getTitle());
+        infoDto.setDescription(entity.getDescription());
+        infoDto.setMinPriceWanted(entity.getMinPriceWanted());
+        infoDto.setImageUrl(entity.getImageURL());
+        infoDto.setStatus(entity.getStatus());
+        return infoDto;
+    }
+}
