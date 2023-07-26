767c26d0e7bada92a2975ce9f10ebc1152097cf6
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-26 15:27:04 +0900

    feat: 회원가입 기능 추가

--- src/main/java/com/be05/market/controller/UserController.java
+++ src/main/java/com/be05/market/controller/UserController.java
@@ -1,20 +1,49 @@
 package com.be05.market.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserDetailsManager userManger;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/login")
-    public String loginPage() {
+    public String login() {
         return "login";
     }
 
     @GetMapping("/profile")
     public String viewProfile() {
         return "profile";
+    }
+
+    @GetMapping("/register")
+    public String signUpForm() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String signUp(
+            @RequestParam("userId") String userId,
+            @RequestParam("password") String password,
+            @RequestParam("password-check") String passwordCheck) {
+        if (password.equals(passwordCheck)) {
+            userManger.createUser(User
+                    .withUsername(userId)
+                    .password(passwordEncoder.encode(password))
+                    .build());
+            return "redirect:/users/login";
+        }
+        return "redirect:/users/register?error";
     }
 }
