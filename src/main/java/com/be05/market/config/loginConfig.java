65bda3385bba428230e6df55f958d81dd4ddc048
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-26 14:04:07 +0900

    feat: loginConfig 추가

---
+++ src/main/java/com/be05/market/config/loginConfig.java
@@ -0,0 +1,52 @@
+package com.be05.market.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class loginConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authHttp -> authHttp
+                        .requestMatchers("/no-auth").permitAll()
+                        .requestMatchers("/re-auth", "/users/profile").authenticated()
+                        .requestMatchers("/").anonymous()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/users/login")
+                        .defaultSuccessUrl("/users/profile")
+                        .failureUrl("/users/login?fail")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/users/logout")
+                        .logoutSuccessUrl("/users/login")
+                );
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder) {
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder.encode("password1"))
+                .build();
+        return new InMemoryUserDetailsManager(user1);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
