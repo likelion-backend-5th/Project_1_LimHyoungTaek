c4ce4f663a06074e48ece284110b3cacfdce6ffb
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-26 22:43:01 +0900

    feat: 테스트 계정 추가, UserRepository에 저장 기능

--- src/main/java/com/be05/market/service/JpaUserDetailsManager.java
+++ src/main/java/com/be05/market/service/JpaUserDetailsManager.java
@@ -2,24 +2,24 @@ package com.be05.market.service;
 
 import com.be05.market.entity.UserEntity;
 import com.be05.market.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
 @Service
-//@RequiredArgsConstructor
 public class JpaUserDetailsManager implements UserDetailsManager {
     private final UserRepository userRepository;
 
-    public JpaUserDetailsManager(
-            UserRepository userRepository,
-            PasswordEncoder passwordEncoder
-    ) {
+    public JpaUserDetailsManager(UserRepository userRepository,
+                                 PasswordEncoder passwordEncoder)
+    {
         this.userRepository = userRepository;
         createUser(User.withUsername("user")
                 .password(passwordEncoder.encode("asdf"))
@@ -27,35 +27,38 @@ public class JpaUserDetailsManager implements UserDetailsManager {
         );
     }
 
-
     @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
         Optional<UserEntity> optionalUser = userRepository.findByUserId(userId);
         if (optionalUser.isEmpty()) throw new UsernameNotFoundException(userId);
 
         UserEntity userEntity = optionalUser.get();
-        return User.withUsername(userEntity.getPassword())
+        return User.withUsername(userEntity.getUserId())
+                .password(userEntity.getPassword())
                 .build();
     }
 
     @Override
     public void createUser(UserDetails user) {
+        if (this.userExists(user.getUsername()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
 
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(user.getUsername());
+        userEntity.setPassword(user.getPassword());
+        this.userRepository.save(userEntity);
     }
 
     @Override
     public void updateUser(UserDetails user) {
-
     }
 
     @Override
     public void deleteUser(String username) {
-
     }
 
     @Override
     public void changePassword(String oldPassword, String newPassword) {
-
     }
 
     @Override
