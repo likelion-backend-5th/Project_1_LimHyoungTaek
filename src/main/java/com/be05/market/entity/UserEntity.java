87fae0d57a87486c87b3250b26a465032257c969
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-26 23:52:57 +0900

    feat: @NotNull -> @Column(nullable = false)

--- src/main/java/com/be05/market/entity/UserEntity.java
+++ src/main/java/com/be05/market/entity/UserEntity.java
@@ -1,7 +1,6 @@
 package com.be05.market.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -12,10 +11,9 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message = "아이디를 입력해주세요.")
     @Column(nullable = false) // unique = true -> sqlite error
     private String userId;
-    @NotNull(message = "비밀번호를 입력해주세요.")
+    @Column(nullable = false)
     private String password;
 
     private String phone;
