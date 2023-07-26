4e17788972ed2b1d0443d6950cc6ea2bbf6df8b6
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-26 14:02:51 +0900

    feat: UserEntity 추가

---
+++ src/main/java/com/be05/market/entity/UserEntity.java
@@ -0,0 +1,21 @@
+package com.be05.market.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull(message = "아이디를 입력해주세요.")
+    private String userId;
+    @NotNull(message = "비밀번호를 입력해주세요.")
+    private String password;
+    private String phoneNumber;
+    private String email;
+    private String address;
+}
