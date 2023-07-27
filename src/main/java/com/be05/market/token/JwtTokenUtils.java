53532c4049fe086d08830ab9e72cb4c227e4f4e8
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-27 10:46:11 +0900

    feat: add JwtTokenUtils  - decoded Token  - generated Token  - validated Token  - return Claims

---
+++ src/main/java/com/be05/market/token/JwtTokenUtils.java
@@ -0,0 +1,55 @@
+package com.be05.market.token;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.sql.Date;
+import java.time.Instant;
+
+@Component
+public class JwtTokenUtils {
+    private final Key signingKey;
+    private final JwtParser jwtParser;
+
+    public JwtTokenUtils(@Value("${jwt.secret}") String jwtSecret) {
+        this.signingKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+        this.jwtParser = Jwts
+                .parserBuilder()
+                .setSigningKey(this.signingKey)
+                .build();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Claims jwtClaims = Jwts.claims()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plusSeconds(3600)));
+
+        return Jwts.builder()
+                .setClaims(jwtClaims)
+                .signWith(signingKey)
+                .compact();
+    }
+
+    public boolean validate(String token) {
+        try {
+            jwtParser.parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Claims parseClaims(String token) {
+        return jwtParser
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
