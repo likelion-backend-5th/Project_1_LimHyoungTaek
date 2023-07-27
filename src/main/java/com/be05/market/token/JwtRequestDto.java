ed6b2204a65026cc26bf26cf25aa8955d9f7adef
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-27 16:21:35 +0900

    feat: Test용 jwt 관련 Dto 제거

--- src/main/java/com/be05/market/token/JwtRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.be05.market.token;
-
-import lombok.Data;
-
-@Data
-public class JwtRequestDto {
-    private String userId;
-    private String password;
-}
--- src/main/java/com/be05/market/token/JwtTokenDto.java
+++
@@ -1,8 +0,0 @@
-package com.be05.market.token;
-
-import lombok.Data;
-
-@Data
-public class JwtTokenDto {
-    private String token;
-}
