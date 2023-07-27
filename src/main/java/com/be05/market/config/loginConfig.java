72840cb639aba031c6d9594d0682bfeea399f41e
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-27 11:10:37 +0900

    feat: formLogin, logout 제거  - sessionManagement 추가

--- src/main/java/com/be05/market/config/loginConfig.java
+++ src/main/java/com/be05/market/config/loginConfig.java
@@ -1,40 +1,38 @@
 package com.be05.market.config;
 
+import com.be05.market.token.JwtTokenFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class loginConfig {
+    private final JwtTokenFilter jwtTokenFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
+        throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authHttp -> authHttp
-                        .requestMatchers(
-                                "/users/logout",
-                                "/users/profile").authenticated()
-                        .requestMatchers("/users/register").anonymous()
-                )
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/users/login")
-                        .defaultSuccessUrl("/users/profile")
-                        .failureUrl("/users/login?fail")
+                        .requestMatchers("/users/login", "/token/issue")
                         .permitAll()
+                        .requestMatchers("/", "/users/register")
+                        .anonymous().anyRequest().authenticated()
                 )
-                .logout(logout -> logout
-                        .logoutUrl("/users/logout")
-                        .logoutSuccessUrl("/users/login")
-                );
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtTokenFilter, AuthorizationFilter.class);
+
         return http.build();
     }
 
