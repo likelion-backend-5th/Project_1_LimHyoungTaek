43de6ca1ec3766e4c7c14a170869001bc2fc867e
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-27 16:22:27 +0900

    feat: 이름 변경 loginConfig -> LoginConfig

--- src/main/java/com/be05/market/config/LoginConfig.java
+++ src/main/java/com/be05/market/config/LoginConfig.java
@@ -4,28 +4,26 @@ import com.be05.market.token.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 @RequiredArgsConstructor
-public class loginConfig {
+public class LoginConfig {
     private final JwtTokenFilter jwtTokenFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-        throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authHttp -> authHttp
                         .requestMatchers("/users/login")
                         .permitAll()
-                        .requestMatchers("/", "/users/register")
+                        .requestMatchers("/", "/users/login", "/users/register")
                         .anonymous().anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManagement -> sessionManagement
@@ -34,10 +32,5 @@ public class loginConfig {
                 .addFilterBefore(jwtTokenFilter, AuthorizationFilter.class);
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
