836a4c1f497e76a4eefb72296687b99a6b2183c6
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-07-28 11:45:22 +0900

    feat: RESTful API로 변경  - GET 부분 제거  - POST /users/register -> dto로 받아오고 기능은 UserService로 넘김 더 수정할 예정

--- src/main/java/com/be05/market/controller/UserController.java
+++ src/main/java/com/be05/market/controller/UserController.java
@@ -1,58 +1,31 @@
 package com.be05.market.controller;
 
+import com.be05.market.dto.ResponseDto;
 import com.be05.market.dto.UserDto;
-import com.be05.market.service.JpaUserDetailsManager;
+import com.be05.market.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-    private final JpaUserDetailsManager jpaUserDetailsManager;
-    private final PasswordEncoder passwordEncoder;
+    private final UserService jpaUserDetailsManager;
+    private final ResponseDto responseDto = new ResponseDto();
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-
-    @PostMapping("/login") @ResponseBody
+    @PostMapping("/login")
     public UserDto generateJwt(@RequestBody UserDto dto) {
         return jpaUserDetailsManager.loginToSaveToken(dto);
     }
 
-    @GetMapping("/profile")
-    public String viewProfile() {
-        return "profile";
-    }
-
-    @GetMapping("/register")
-    public String signUpForm() {
-        return "register";
-    }
-
     @PostMapping("/register")
-    public String signUp(
-            @RequestParam("userId") String userId,
-            @RequestParam("password") String password,
-            @RequestParam("password-check") String passwordCheck,
-            @RequestParam("phone") String phone,
-            @RequestParam("email") String email,
-            @RequestParam("address") String address) {
-        if ((!userId.isEmpty() && !password.isEmpty())
-                && password.equals(passwordCheck)) {
-            jpaUserDetailsManager.createUser(UserDto.builder()
-                    .userId(userId)
-                    .password(passwordEncoder.encode(password))
-                    .phone(phone)
-                    .email(email)
-                    .address(address)
-                    .build());
-            return "redirect:/users/login";
+    public ResponseDto signUp(@RequestBody UserDto dto) {
+        if ((!dto.getUserId().isEmpty() && !dto.getPassword().isEmpty())) {
+            jpaUserDetailsManager.createUser(dto);
+            responseDto.setMessage("회원가입 성공");
+            return responseDto;
         }
-        return "redirect:/users/register?error";
+        responseDto.setMessage("회원가입 실패");
+        return responseDto;
     }
 }
