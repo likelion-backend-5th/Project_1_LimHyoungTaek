56197ebcb59f2681c6f7718544de655d78cd46ae
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-02 23:17:54 +0900

    feat: html에서 작동안해서 롤백

--- src/main/java/com/be05/market/controller/UserController.java
+++ src/main/java/com/be05/market/controller/UserController.java
@@ -19,15 +19,9 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public ResponseDto signUp(@RequestBody UserDto dto,
-                              @RequestParam("passwordCheck") String passwordCheck) {
-        // users/register/view 에서 이미 처리했지만 Postman 입력 대비
+    public ResponseDto signUp(@RequestBody UserDto dto) {
         if (dto.getUserId().isEmpty() || dto.getPassword().isEmpty())
             responseDto.setMessage("아이디 혹은 비밀번호를 입력해주세요.");
-        // json 데이터로 다 받아오고 싶지만 내 지식의 한에서는 entity에 추가해야하기 때문에
-        // 그냥 form-data로 받아오는 것으로 수정
-        if (!dto.getPassword().equals(passwordCheck))
-            responseDto.setMessage("비밀번호가 일치하지 않습니다. 비밀번호를 확인해주세요.");
         else {
             jpaUserDetailsManager.createUser(dto);
             responseDto.setMessage("회원가입 성공");
